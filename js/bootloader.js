00c9d2044019badcfe6e2d1e85734ce091c716a6
terminator9 <73266537+terminator9@users.noreply.github.com>
2020-11-26 03:02:00 -0400

    Update bootloader.js

--- js/bootloader.js
+++ js/bootloader.js
@@ -7,7 +7,7 @@ class Bootloader extends Phaser.Scene {
             this.scene.start("sceneA");
         });
         this.load.image("pasto", "assets/thepastov3.png");
-        this.load.image("ball","assets/camacho_c.png");
+        this.load.image("ball","assets/Camacho_c.png");
         this.load.image("izquierda","assets/left_pallete.png");
         this.load.image("derecha","assets/right_pallete.png");
         this.load.image("separador","assets/separator.png");
